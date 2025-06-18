fdce6d8cb444e0267d09a3912ff6d104c9ea8aa8
Madhuri Ummadisingh <umadhuri1306@gmail.com>
2025-06-18 18:22:10 +0530

    Add files via upload

---
+++ Homepage.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div style={{ textAlign: "center", maxWidth: "400px" }}>
+      <h1 style={{ fontSize: "2.5rem", marginBottom: "15px" }}>
+        Welcome to Color Picker Tool 🎨
+      </h1>
+      <p style={{ fontSize: "1.2rem", marginBottom: "10px" }}>
+        Pick a color 🎨, paint your world ✨
+      </p>
+      <p style={{ fontSize: "1rem", color: "#444" }}>
+        Click the button below to start picking colors.
+      </p>
+      
+    </div>
+  );
+};
+
+export default Homepage;
+
